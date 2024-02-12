45e8bb2e9a8c22156318a56c1b4cdc346e1d4ab0
Marco Barroso <alu0101386560@ull.edu.es>
2024-02-12 12:50:57 +0000

    primera versión del configurador, funcionando la instalación de django, creación del proyecto y borrar el proyecto

--- src/configurator.py
+++ src/configurator.py
@@ -1,12 +1,19 @@
 # script python encargado de configurar el proyecto
 
 import subprocess
-import platform
 import os
 import shutil
 
 # pip install -r requirements.txt
-# def instalar_django():
+def instalar_django():
+  try:
+    # Instalar Django
+    print("Instalando Django...")
+    subprocess.run(["pip", "install", "-r", "requirements.txt"])
+    print("Django instalado correctamente.")
+  except Exception as e:
+    print(f"Error al instalar Django: {str(e)}")
+
 
 def crear_proyecto():
   try:
@@ -27,6 +34,20 @@ def crear_proyecto():
     print(f"Error al crear el proyecto Django: {str(e)}")
 
 
+def borrar_proyecto():
+  try:
+    # Obtener la ruta del directorio de trabajo actual
+    directorio_actual = os.getcwd()
+    print("Directorio actual:", directorio_actual)
+    # Borrar el proyecto Django
+    print("Borrando proyecto Django...")
+    proyecto_dir = os.path.join(directorio_actual, "django_test")  # Reemplaza con el nombre del proyecto
+    shutil.rmtree(proyecto_dir)
+    print("Proyecto Django borrado correctamente.")
+  except Exception as e:
+    print(f"Error al borrar el proyecto Django: {str(e)}")
+
+
 # def arrancar_servidor():
 #   # Arrancar servidor de desarrollo
 #   print("Arrancando servidor de desarrollo...")
@@ -43,30 +64,3 @@ def crear_proyecto():
 #     print(f"Error al desinstalar Django: {str(e)}")
 
 
-
-def borrar_proyecto():
-  try:
-    # Obtener la ruta del directorio de trabajo actual
-    directorio_actual = os.getcwd()
-    print("Directorio actual:", directorio_actual)
-    # Borrar el proyecto Django
-    print("Borrando proyecto Django...")
-    proyecto_dir = os.path.join(directorio_actual, "django_test")  # Reemplaza con el nombre del proyecto
-    shutil.rmtree(proyecto_dir)
-    print("Proyecto Django borrado correctamente.")
-  except Exception as e:
-    print(f"Error al borrar el proyecto Django: {str(e)}")
-
-
-# if __name__ == "__main__":
-#   # 1. Instalar Django si no está instalado
-#   instalar_django()
-
-#   # 2. Comprobar versión de Django instalada
-#   comprobar_version_django()
-
-#   # 3. Crear proyecto Django
-#   crear_proyecto()
-    
-crear_proyecto()
-# borrar_proyecto()
