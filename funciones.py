3cfed45cb2d9390d9e8668bd1034d024b2f55fdb
Marco Barroso <alu0101386560@ull.edu.es>
2024-02-12 13:06:50 +0000

    actualización de programa principal y creación de un fichero funciones.py, encargado de alojar las funciones necesarias para switchear el menú principal del programa

---
+++ funciones.py
@@ -0,0 +1,31 @@
+from lectura import leerFichero
+from configurador import crear_proyecto, instalar_django, borrar_proyecto
+
+
+def mi_switch(opcion):
+  if opcion == 0:
+    # terminar la ejecución del programa
+    print("salir")
+  elif opcion == 1:
+    # utilizar las funciones del configurador.py
+    print("configurar proyecto")
+  elif opcion == 2:
+    # leer el fichero
+    print("Lectura de fichero")
+  elif opcion == 3:
+    print("Borrar proyecto django")
+
+
+
+
+
+# # fichero = "./ejemplo.json"
+# fichero = "";
+# while fichero != "salir":
+#   fichero = input("Introduce el nombre del fichero: ")
+#   if fichero != "salir":
+#     miDiccionario = leerFichero(fichero, tiposGenericos, tiposEspecificos)
+#     print("imprimiendo diccionario:")
+#     print(miDiccionario)
+#   else: 
+#     exit
--- src/main.py
+++ src/main.py
@@ -1,17 +1,30 @@
-from lectura import leerFichero 
+from funciones import mi_switch
 
 tiposGenericos = ["text", "number", "multipleChoice", "dropdown", "checkbox"]
 tiposEspecificos = ["email", "dni", "phoneNumber", "date", "specialField"]
 
 miDiccionario = {}
 
-# fichero = "./ejemplo.json"
-fichero = "";
-while fichero != "salir":
-  fichero = input("Introduce el nombre del fichero: ")
-  if fichero != "salir":
-    miDiccionario = leerFichero(fichero, tiposGenericos, tiposEspecificos)
-    print("imprimiendo diccionario:")
-    print(miDiccionario)
-  else: 
-    exit
+
+def menu_principal():
+  print("0. Salir")
+  print("1. Configurar proyecto Django")
+  print("2. Lectura de fichero JSON")
+  print("3. Borrar proyecto Django")
+  print("\nNOTA: Si no se ha configurado el proyecto Django, no se podrá leer el fichero JSON.\n")
+  # Controlamos input, seguimos pidiendo hasta que se introduzca un número válido
+  while True:
+    try:
+      opcion = int(input("Introduce una opción: "))
+      if opcion >= 0 and opcion <= 3:
+        return opcion
+      else:
+        print("Introduce una opción válida")
+    except ValueError:
+      print("Introduce una opción válida")
+
+
+
+
+opcion = menu_principal()
+mi_switch(opcion)
