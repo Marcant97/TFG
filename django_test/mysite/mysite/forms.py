097ef0a0c480618619f950011ac99e3acc75566c
Marco Barroso <alu0101386560@ull.edu.es>
2024-03-02 11:04:25 +0000

    eliminación del proyecto de prueba para continuar con el desarrollo

--- django_test/mysite/mysite/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import TuModelo
-
-class TuFormulario(forms.ModelForm):
-    class Meta:
-        model = TuModelo
-        fields = ['campo1', 'campo2']
--- django_test/mysite/mysite/models.py
+++
@@ -1,8 +0,0 @@
-# models.py
-
-from django.db import models
-
-class TuModelo(models.Model):
-    campo1 = models.CharField(max_length=100)
-    campo2 = models.EmailField()
-    # Agrega más campos según sea necesario
