14803c8b1c5c5c75901e4ab2c07d66c8f001f5e7
Marco Barroso <alu0101386560@ull.edu.es>
2024-03-31 20:43:09 +0100

    modificaciones manuales para mejorar la demo, como añadir plantillas y vistas para el home y para después de enviar el formulario.

--- django_test/mysite/mysite/urls.py
+++ django_test/mysite/mysite/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.home, name='home'),
     path('formulario/', views.mi_vista, name='mi_vista'),
-    path('admin/', admin.site.urls)
+    path('admin/', admin.site.urls),
+    path('enviar/', views.vista_enviar, name='vista_enviar')
 ]
--- django_test/mysite/mysite/views.py
+++ django_test/mysite/mysite/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from .forms import TuFormulario
 from .models import TuModelo
+from django.shortcuts import render, redirect
+
+# nueva vista para la página "/".
+def home(request):
+    return render(request, 'home.html')
+
 
 def mi_vista(request):
     if request.method == 'POST':
@@ -14,6 +20,12 @@ def mi_vista(request):
             form.instance.introducetunmerodetelfono = numero_completo
             form.save()
             # Realiza acciones adicionales después de guardar el formulario
+            return redirect('vista_enviar')
     else:
         form = TuFormulario()
     return render(request, 'mi_template.html', {'form': form})
+
+
+# nueva vista para mostrar una vez que se envíe el formulario
+def vista_enviar(request):
+    return render(request, 'enviado.html')  # Suponiendo que has creado este HTML
