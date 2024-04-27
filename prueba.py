e0f4b9e97ed75afbbf2441f64353c9732326d92c
Marco Barroso <alu0101386560@ull.edu.es>
2024-04-27 08:46:48 +0100

    muy buenos avances, faltaría mejorar un poco el estilo, centrar el contendio y terminar de agregar el resto de campos

--- prueba.py
+++ prueba.py
@@ -7,13 +7,15 @@ import json
 tipos_pregunta = ["texto", "numero"]
 
 limite_entry = None
-limite_superior_entry = None
-limite_inferior_entry = None
+valor_maximo_entrada = None
+valor_minimo_entrada = None
+
+
 
 def mostrar_campos_adicionales(tipo_seleccionado):
     global limite_entry
-    global limite_superior_entry
-    global limite_inferior_entry
+    global valor_maximo_entrada
+    global valor_minimo_entrada
 
     # Limpiar cualquier widget anterior
     for widget in campos_adicionales_frame.winfo_children():
@@ -21,23 +23,32 @@ def mostrar_campos_adicionales(tipo_seleccionado):
 
     if tipo_seleccionado == "texto":
         # Campo opcional para el límite
-        limite_label = tk.Label(campos_adicionales_frame, text="limite:")
+        limite_label = tk.Label(campos_adicionales_frame, text="Límite de caracteres:")
         limite_label.grid(row=0, column=0, padx=5, pady=5)
         limite_entry = tk.Entry(campos_adicionales_frame)
         limite_entry.grid(row=0, column=1, padx=5, pady=5)
 
+        limite_label.configure(bg=root.cget('bg')) # fondo
+
     elif tipo_seleccionado == "numero":
-        # campo opcional para el límite superior
-        limite_superior_label = tk.Label(campos_adicionales_frame, text="limite superior:")
-        limite_superior_label.grid(row=0, column=0, padx=5, pady=5)
-        limite_superior_entry = tk.Entry(campos_adicionales_frame)
-        limite_superior_entry.grid(row=0, column=1, padx=5, pady=5)
 
         # campo opcional para el límite inferior
-        limite_inferior_label = tk.Label(campos_adicionales_frame, text="limite inferior:")
-        limite_inferior_label.grid(row=1, column=0, padx=5, pady=5)
-        limite_inferior_entry = tk.Entry(campos_adicionales_frame)
-        limite_inferior_entry.grid(row=1, column=1, padx=5, pady=5)
+        valor_minimo_label = tk.Label(campos_adicionales_frame, text="Valor mínimo:")
+        valor_minimo_label.grid(row=1, column=0, padx=5, pady=5)
+        valor_minimo_entrada = tk.Entry(campos_adicionales_frame)
+        valor_minimo_entrada.grid(row=1, column=1, padx=5, pady=5)
+
+        valor_minimo_label.configure(bg=root.cget('bg')) # fondo
+
+        # campo opcional para el límite superior
+        valor_maximo_label = tk.Label(campos_adicionales_frame, text="Valor máximo:")
+        valor_maximo_label.grid(row=0, column=0, padx=5, pady=5)
+        valor_maximo_entrada = tk.Entry(campos_adicionales_frame)
+        valor_maximo_entrada.grid(row=0, column=1, padx=5, pady=5)
+
+        valor_maximo_label.configure(bg=root.cget('bg')) # fondo
+
+
 
 
 
@@ -54,16 +65,16 @@ def agregar_pregunta():
             campos_adicionales["limite"] = limite
 
     elif tipo_seleccionado == "numero":
-        limite_superior = limite_superior_entry.get()
-        if limite_superior:
-            campos_adicionales["limite_superior"] = limite_superior
+        valor_maximo = valor_maximo_entrada.get()
+        if valor_maximo:
+            campos_adicionales["valorMinimo"] = int(valor_maximo)
 
-        limite_inferior = limite_inferior_entry.get()
-        if limite_inferior:
-            campos_adicionales["limite_inferior"] = limite_inferior
+        valor_minimo = valor_minimo_entrada.get()
+        if valor_minimo:
+            campos_adicionales["valorMaximo"] = int(valor_minimo)
 
     # Guardar la pregunta y los campos adicionales en la lista
-    preguntas.append({"pregunta": pregunta, "tipo_pregunta": tipo_seleccionado, **campos_adicionales})
+    preguntas.append({"tipo": tipo_seleccionado, "titulo": pregunta, **campos_adicionales})
 
     # Limpiar campos de entrada
     tipo_pregunta_combobox.set("")
@@ -71,8 +82,8 @@ def agregar_pregunta():
     if tipo_seleccionado == "texto":
         limite_entry.delete(0, tk.END)
     elif tipo_seleccionado == "numero":
-        limite_superior_entry.delete(0, tk.END)
-        limite_inferior_entry.delete(0, tk.END)
+        valor_maximo_entrada.delete(0, tk.END)
+        valor_minimo_entrada.delete(0, tk.END)
 
 
 
@@ -102,7 +113,7 @@ root.configure(bg="white")
 
 
 # campo tipo
-tipo_pregunta_label = tk.Label(root, text="Tipo de pregunta:")
+tipo_pregunta_label = tk.Label(root, text="Tipo de pregunta:*")
 tipo_pregunta_label.pack(padx=5, pady=5)
 tipo_pregunta_combobox = ttk.Combobox(root, values=tipos_pregunta, state="readonly")
 tipo_pregunta_combobox.pack(padx=5, pady=5)
@@ -110,7 +121,7 @@ tipo_pregunta_combobox.pack(padx=5, pady=5)
 tipo_pregunta_label.configure(bg=root.cget('bg')) # fondo
 
 # campo pregunta
-pregunta_label = tk.Label(root, text="Pregunta:")
+pregunta_label = tk.Label(root, text="Pregunta:*")
 pregunta_label.pack(padx=5, pady=5)
 pregunta_entry = tk.Entry(root)
 pregunta_entry.pack(padx=5, pady=5)
@@ -120,6 +131,7 @@ pregunta_label.configure(bg=root.cget('bg')) # fondo
 
 # Frame para los campos adicionales
 campos_adicionales_frame = tk.Frame(root)
+campos_adicionales_frame = tk.Frame(root, bg=root.cget('bg')) # fondo
 campos_adicionales_frame.pack(padx=5, pady=5, fill="x")
 
 # Asociar la función mostrar_campos_adicionales al evento de selección de un elemento en el Combobox
