9a9f261a68f6293f6a2b57ca72b15d7e0239b98c
Marco Barroso <alu0101386560@ull.edu.es>
2024-05-20 12:43:49 +0100

    nuevos proyectos de prueba

---
+++ proyecto_django/mi_sitio_1/mi_sitio_1/forms.py
@@ -0,0 +1,43 @@
+from django import forms
+from .models import TuModelo
+
+from django.utils import timezone
+from datetime import datetime
+class TuFormulario(forms.ModelForm):
+    aceptoelreglamentodelaprueba = forms.BooleanField(label='Acepto el reglamento de la prueba *', required=True)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        min_date_introducetufechadenacimiento = datetime.strptime('01/01/1910', '%d/%m/%Y').strftime('%Y-%m-%d')
+        max_date_introducetufechadenacimiento = datetime.strptime('31/12/2010', '%d/%m/%Y').strftime('%Y-%m-%d')
+        self.fields['introducetufechadenacimiento'].widget.attrs['min'] = min_date_introducetufechadenacimiento
+        self.fields['introducetufechadenacimiento'].widget.attrs['max'] = max_date_introducetufechadenacimiento
+    class Meta:
+        model = TuModelo
+        fields = [
+            'introducetunombreyapellidos',
+            'introducetufechadenacimiento',
+            'introducetuedad',
+            'introducetualu',
+            'prefijo_introducetunmerodetelfono',
+            'introducetunmerodetelfono',
+            'introducetudireccindecorreoelectrnico',
+            'introducetudni',
+            'seleccionatutalladecamiseta',
+            'aceptoelreglamentodelaprueba',
+        ]
+        labels = {
+            'introducetunombreyapellidos': 'Introduce tu nombre y apellidos:',
+            'introducetufechadenacimiento': 'Introduce tu fecha de nacimiento:',
+            'introducetuedad': 'Introduce tu edad',
+            'introducetualu': 'Introduce tu alu',
+            'prefijo_introducetunmerodetelfono': 'Prefijo telefónico',
+            'introducetunmerodetelfono': 'Introduce tu número de teléfono:',
+            'introducetudireccindecorreoelectrnico': 'Introduce tu dirección de correo electrónico:',
+            'introducetudni': 'Introduce tu DNI:',
+            'seleccionatutalladecamiseta': 'Selecciona tu talla de camiseta:',
+        }
+        widgets = {
+            'introducetufechadenacimiento': forms.DateInput(
+                attrs={'type': 'date', 'class': 'form-control'
+                })
+        }
